deb6d8b9c4f55fd506b34201c4975b10b7eb9446
rostyslavmoskal <moskalrostyslav@gmail.com>
2023-04-05 18:11:45 -0700

    feat: add ClientVendor entity and enum

---
+++ src/main/java/com/cydeo/entity/ClientVendor.java
@@ -0,0 +1,30 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.ClientVendorType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients_vendors")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ClientVendor extends BaseEntity {
+    private String clientVendorName;
+    private String phone;
+    private String website;
+
+    @Enumerated(EnumType.STRING)
+    private ClientVendorType clientVendorType;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+}

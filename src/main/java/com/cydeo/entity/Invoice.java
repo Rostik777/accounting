d8175df8e067fc3010019e931c7c97adbcd1d9a0
rostyslavmoskal <moskalrostyslav@gmail.com>
2023-04-05 18:57:52 -0700

    feat: add Invoice entity and Invoice's Status and Type enums

---
+++ src/main/java/com/cydeo/entity/Invoice.java
@@ -0,0 +1,35 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "invoices")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Invoice extends BaseEntity {
+    private String invoiceNo;
+
+    @Enumerated(EnumType.STRING)
+    private InvoiceStatus invoiceStatus;
+
+    @Enumerated(EnumType.STRING)
+    private InvoiceType invoiceType;
+
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_vendor_id")
+    private ClientVendor clientVendor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+}

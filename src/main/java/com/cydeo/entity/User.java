0c105d2438b5cbddf669845b348a99516d841d0e
rostyslavmoskal <moskalrostyslav@gmail.com>
2023-04-05 17:25:22 -0700

    feat: add User entity

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,34 @@
+package com.cydeo.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User extends BaseEntity {
+    @Column(unique = true)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private boolean enabled;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    Company company;
+}

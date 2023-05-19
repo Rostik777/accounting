c2590595379eb49651edfc9b3b85e56b874456c5
rostyslavmoskal <moskalrostyslav@gmail.com>
2023-05-19 00:00:11 -0700

    fix: char was accidentally removed

--- src/main/java/com/cydeo/controller/PurchaseInvoiceController.java
+++ src/main/java/com/cydeo/controller/PurchaseInvoiceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/purchaseInvoice")
+@RequestMapping("/purchaseInvoices")
 public class PurchaseInvoiceController {
     private final InvoiceService invoiceService;
 
